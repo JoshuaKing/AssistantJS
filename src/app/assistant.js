8f7427922cc3ee19105aaf291b78274e9af49613
Josh King <josking@expedia.com>
2016-02-19 18:06:19 +1000

    Encrypt/Decrypt Config values

--- src/app/assistant.js
+++ src/app/assistant.js
@@ -1,11 +1,29 @@
 var crypto = require('crypto');
 var process = require('process');
+var prompt = require('prompt-sync').prompt;
+var fs = require("fs-extra");
 var config = require(process.cwd() + "/config/configuration.json");
 var key = require(process.cwd() + "/config/aes.json").key;
 
 console.log("AssistantJS");
 
-var decipher = crypto.createDecipher("aes256", key);
-decipher.update(config.name, 'base64', 'utf8');
-console.log(decipher.final('utf8'));
+function decrypt(encrypted) {
+    var decipher = crypto.createDecipher("aes256", key);
+    decipher.update(encrypted, 'base64', 'utf8');
+    return decipher.final('utf8');
+}
+
+function encrypt(plaintext) {
+    var cipher = crypto.createCipher("aes256", key);
+    cipher.update(plaintext);
+    return cipher.final('base64');
+}
+
+
+console.log("Old Name: " + decrypt(config.name));
+console.log("New Name?");
+config.name = encrypt(prompt());
+
+console.log(JSON.stringify(config));
+fs.writeFile(process.cwd() + "/config/configuration.json", JSON.stringify(config));
 
