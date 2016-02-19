d36dcba5f6dda7a8bcb7612815801c96ee81326b
Josh King <josking@expedia.com>
2016-02-19 17:37:11 +1000

    Decryption of configuration

--- src/app/assistant.js
+++ src/app/assistant.js
@@ -1,6 +1,11 @@
 var crypto = require('crypto');
 var process = require('process');
 var config = require(process.cwd() + "/config/configuration.json");
+var key = require(process.cwd() + "/config/aes.json").key;
 
 console.log("AssistantJS");
+
+var decipher = crypto.createDecipher("aes256", key);
+decipher.update(config.name, 'base64', 'utf8');
+console.log(decipher.final('utf8'));
 
